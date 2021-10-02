213ac3ce9e9ac0ae7396a4542a9b951ab424d69f
evanfreymiller <ejf1990@gmail.com>
2021-10-02 19:47:17 -0400

    added video back

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import './App.css';
 import { Box } from '@chakra-ui/react';
 
 function App() {
-  window.addEventListener('load', function () {
-    var newVideo = document.getElementById('videoPlayer');
-    console.log(newVideo);
-    newVideo.addEventListener('ended', function () {
-      this.currentTime = 0;
-      this.play();
-    }, false);
-    newVideo.play();
-  });
-
   return (
     <Box height={window.innerHeight} width={window.innerWidth}>
-      {/* <video id="videoPlayer" autoPlay controls={false} width="100%" loop muted playsInline>
+      <video id="videoPlayer" autoPlay controls={false} width="100%" loop muted playsInline>
         <source src="/videos/troy.MOV" type="video/mp4" />
         Sorry, your browser doesn't support embedded videos.
-      </video> */}
-      test
+      </video>
     </Box>
   );
 }
