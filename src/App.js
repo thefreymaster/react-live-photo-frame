e5482dda508b32dfc9ddbf4ff4023d2a007218c3
evanfreymiller <ejf1990@gmail.com>
2023-01-04 20:32:25 -0500

    build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import React from 'react';
-import { Box, IconButton } from '@chakra-ui/react';
-import { Routes } from './routes';
-import { useIsDay } from './hooks/index';
-import { GiRetroController } from 'react-icons/gi';
-import { Link, useHistory, useLocation } from 'react-router-dom';
-import { isMobile } from 'react-device-detect';
+import "./App.css";
+import React from "react";
+import { Box, IconButton } from "@chakra-ui/react";
+import { Routes } from "./routes";
+import { useIsDay } from "./hooks/index";
+import { GiRetroController } from "react-icons/gi";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 import { io } from "socket.io-client";
-import { Provider } from './providers';
-import { IoIosRefresh } from 'react-icons/io';
+import { Provider } from "./providers";
+import { IoIosRefresh } from "react-icons/io";
 
 const socket = io(`http://${process.env.REACT_APP_SERVER_IP}:4000`);
 
@@ -16,23 +16,23 @@ const App = () => {
   const isDay = useIsDay();
   const histroy = useHistory();
   const location = useLocation();
-  const [device, setDevice] = React.useState(localStorage.getItem('device'));
+  const [device, setDevice] = React.useState(localStorage.getItem("device"));
 
   const getBackgroundColor = () => {
-    if (location.pathname.includes('holiday')) {
-      return '#fff3cb'
+    if (location.pathname.includes("holiday")) {
+      return "#fff3cb";
     }
-    if (location.pathname.includes('weather')) {
-      return isDay ? '#e2e2e2' : '#000'
+    if (location.pathname.includes("weather")) {
+      return isDay ? "#e2e2e2" : "#000";
     }
-    return isDay ? '#fff' : '#000'
-  }
+    return isDay ? "#fff" : "#000";
+  };
 
   React.useEffect(() => {
     socket.on("change_view", (view) => {
-      console.log(view)
-      if (device === 'frame') {
-        if (view === 'reload') {
+      console.log(view);
+      if (device === "frame") {
+        if (view === "reload") {
           window.location.reload();
         }
         histroy.push(`/${view}`);
@@ -46,39 +46,54 @@ const App = () => {
         height={window.innerHeight}
         width={window.innerWidth}
         display="flex"
-        alignItems={isMobile ? "flex-start" : "center"}
-        justifyContent='center'
+        alignItems="center"
+        justifyContent="center"
         style={{
           backgroundColor: getBackgroundColor(),
-          overflow: 'hidden',
+          overflow: "hidden",
         }}
         pt={isMobile && 8}
       >
         <Routes socket={socket} device={device} />
-        {!location.pathname.includes('controller') && !location.pathname.includes('device') && <Link to="/controller">
-          <IconButton colorScheme="gray" size="lg" icon={<GiRetroController />} style={{
-            position: 'fixed',
+        {!location.pathname.includes("controller") &&
+          !location.pathname.includes("device") && (
+            <Link to="/controller">
+              <IconButton
+                colorScheme="gray"
+                size="lg"
+                icon={<GiRetroController />}
+                style={{
+                  position: "fixed",
+                  bottom: 0,
+                  right: 0,
+                  borderRadius: "5px 0px 0px",
+                  minHeight: 70,
+                  minWidth: 70,
+                }}
+              />
+            </Link>
+          )}
+        <IconButton
+          colorScheme="gray"
+          size="lg"
+          icon={<IoIosRefresh />}
+          style={{
+            position: "fixed",
             bottom: 0,
-            right: 0,
-            borderRadius: '5px 0px 0px',
+            left: 0,
+            borderRadius: "0px 5px 0px 0px",
             minHeight: 70,
-            minWidth: 70
-          }} />
-        </Link>}
-        <IconButton colorScheme="gray" size="lg" icon={<IoIosRefresh />} style={{
-          position: 'fixed',
-          bottom: 0,
-          left: 0,
-          borderRadius: '0px 5px 0px 0px',
-          minHeight: 70,
-          minWidth: 70,
-        }}
-          onClick={() => device === 'controller' ? socket.emit('change', 'refresh') : window.location.reload(true)}
-
+            minWidth: 70,
+          }}
+          onClick={() =>
+            device === "controller"
+              ? socket.emit("change", "refresh")
+              : window.location.reload(true)
+          }
         />
       </Box>
     </Provider>
   );
-}
+};
 
 export default App;
--- src/components/DigitalClock/index.tsx
+++ src/components/DigitalClock/index.tsx
@@ -1,38 +1,46 @@
-import { Box } from '@chakra-ui/layout';
-import { Fade, Text } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/layout";
+import { Fade, Text } from "@chakra-ui/react";
 
-import React from 'react';
-import { useIsDay } from '../../hooks/index';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useIsDay } from "../../hooks/index";
+import { useHistory } from "react-router-dom";
 
-export const DigitalClock = (props: { fontSize?: string, hideTime?: boolean }) => {
-    const history = useHistory();
-    const isDay = useIsDay();
-    let now = new Date();
-    const [time, setTime] = React.useState(now.toLocaleTimeString());
-    React.useLayoutEffect(() => {
-        const getTime = () => {
-            setTimeout(() => {
-                if (new Date().toLocaleTimeString().includes(':00:00')) {
-                    history.push(`/announcement${history.location.pathname}`)
-                }
-                else {
-                    setTime(new Date().toLocaleTimeString());
-                    getTime();
-                }
-            }, 1000);
+export const DigitalClock = (props: {
+  fontSize?: string;
+  hideTime?: boolean;
+}) => {
+  const history = useHistory();
+  const isDay = useIsDay();
+  let now = new Date();
+  const [time, setTime] = React.useState(now.toLocaleTimeString());
+  React.useLayoutEffect(() => {
+    const getTime = () => {
+      setTimeout(() => {
+        if (new Date().toLocaleTimeString().includes(":00:00")) {
+          history.push(`/announcement${history.location.pathname}`);
+        } else {
+          setTime(new Date().toLocaleTimeString());
+          getTime();
         }
-        getTime();
-    }, []);
+      }, 1000);
+    };
+    getTime();
+  }, []);
 
-    if(props.hideTime){
-        return <></>;
-    }
-    return (
-        <Fade in>
-            <Box>
-                <Text fontFamily="'Roboto Mono', monospace" fontSize={props.fontSize || "8em"} color={isDay ? 'black' : 'white'}>{time}</Text>
-            </Box>
-        </Fade>
-    )
-}
+  if (props.hideTime) {
+    return <></>;
+  }
+  return (
+    <Fade in>
+      <Box>
+        <Text
+          fontFamily="'Roboto Mono', monospace"
+          fontSize={props.fontSize || "6em"}
+          color={isDay ? "black" : "white"}
+        >
+          {time}
+        </Text>
+      </Box>
+    </Fade>
+  );
+};
